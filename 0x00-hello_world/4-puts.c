b425c17b29a196614b45ff4f05db2851c07a9e27
catherinekimani <catherinekimani882@gmail.com>
2023-02-16 11:37:15 -0800

    Fix error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    /* Use the puts function to print the message */
     puts("Programming is like building a multilingual puzzle\n");
-
-    /* Return 0 to indicate successful completion */
     return (0);
 }
 
