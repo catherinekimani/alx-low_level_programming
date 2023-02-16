f42c783cc980a542d952f53f0b3446b8ecbe3479
catherinekimani <catherinekimani882@gmail.com>
2023-02-16 11:22:44 -0800

    Update file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #!/bin/bash
-# include <stdio.h>
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void){
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
