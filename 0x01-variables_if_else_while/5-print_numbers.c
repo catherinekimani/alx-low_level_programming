582668e4d8ffe47fd085168c91181a033339a40f
catherinekimani <catherinekimani882@gmail.com>
2023-02-17 13:49:04 -0800

    Add program to print single digit numbers from base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
