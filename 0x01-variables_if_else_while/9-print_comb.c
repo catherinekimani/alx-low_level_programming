b8c52b945cc9f101fdaec8439af464398c14292d
catherinekimani <catherinekimani882@gmail.com>
2023-02-18 03:27:50 -0800

    Add program to print all single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+/**
+ * main - entry point
+ * program prints all possible combinations of single digi numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
