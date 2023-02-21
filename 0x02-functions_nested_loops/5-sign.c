6cd55297c75dcb5c215ffecbae8c66eca6e3b6f3
catherinekimani <catherinekimani882@gmail.com>
2023-02-21 12:16:11 -0800

    add a return statement

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,20 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		_putchar('\n');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar('\n');
-		return (-1);
 	}
-	return (0);
+	return (-1);
 }
