7ba4d1a4032a8f9c66a4c4ef63667453ed5f7486
catherinekimani <catherinekimani882@gmail.com>
2023-02-21 13:24:22 -0800

    Create function to print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute value of a given integer
+ * @n: the integer to be checked
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
