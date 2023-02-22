959ab8527c5f15d757be37ca0c87a0a02fa4889c
catherinekimani <catherinekimani882@gmail.com>
2023-02-21 21:35:39 -0800

    Edi function - remove trailing spaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,29 @@
 */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		printf("%d", n);
-		if (n  != 98)
+		while (n <= 98)
 		{
-			printf(",");
-			printf(" ");
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
-		n++;
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
 	}
 	printf("\n");
 }
-
