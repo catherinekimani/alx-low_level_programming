946432348e086e7579b1e741305e56792cae7ea2
catherinekimani <catherinekimani882@gmail.com>
2023-02-24 04:21:38 -0800

    Create program that prints numbers from 1 - 100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+# include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * program that prints the numbers from 1 to 100
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
