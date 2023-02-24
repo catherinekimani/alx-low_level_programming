225df59a7c1a2ecf3ac30b1bccfa308768526b2e
catherinekimani <catherinekimani882@gmail.com>
2023-02-24 04:56:44 -0800

    Update file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,14 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= i; j++)
-		{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
 			_putchar('#');
-		}
-		_putchar('\n');
+		if (i < (size - 1))
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	printf("%d", i);
+
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
