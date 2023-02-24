bc1a1d2233faf3a6d568b86c71bc47575aa66805
catherinekimani <catherinekimani882@gmail.com>
2023-02-24 05:04:59 -0800

    Add paranthensis on if statement to print correct output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * program that prints the numbers from 1 to 100
- * Return:0
+ * main - prints Buzz each numbers of 3 and 5.
+ * Return: Always 0.
  */
 int main(void)
 {
 	int i;
 
 	i = 1;
-	printf("%d", i);
-
+	printf("%d", n);
 	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
-
 	printf("\n");
-
 	return (0);
 }
