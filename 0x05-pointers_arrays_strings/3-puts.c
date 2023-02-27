b46c50de8be16a57e01ec3516129cc250bb79edb
catherinekimani <catherinekimani882@gmail.com>
2023-02-27 11:55:25 -0800

    Feat: Create function to print a strin to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+# include "main.h"
+# include <stdion.h>
+/**
+ * _puts - function that prints a string,
+ * followed by a new line, to stdout.
+ * @str: string to be tested
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	puts(str); /* writing string to stdout*/
+}
