b3ce770f0fddc577d13d80afa4b17f61cb345334
catherinekimani <catherinekimani882@gmail.com>
2023-02-27 11:38:02 -0800

    Feat: Create function to return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ *  _strlen -  function that returns the length of a string
+ *  @s: Character to be checked
+ *  Return: 0;
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
