4afcdd795b926b6a03051002501301ebfc9c2bf0
catherinekimani <catherinekimani882@gmail.com>
2023-02-27 12:26:43 -0800

    Fix bug causing compilation error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 # include "main.h"
-#include <stdio.h>
-#include <string.h>
+# include <stdio.h>
+# include <string.h>
 /**
  * print_rev - function that prints a string,
  * in reverse, followed by a new line.
- * @s - Character to be checked
+ * @s: Character to be checked
  * Return: 0
  */
 void print_rev(char *s)
@@ -12,7 +12,7 @@ void print_rev(char *s)
 	int length = strlen(s);
 	int a;
 
-	for(a = length - 1;a >= 0; a--;)
+	for (a = length - 1; a >= 0; a--)
 	{
 		printf("%c", s[i]);
 	}
