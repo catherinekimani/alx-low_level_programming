bb66a7ec3ccc952b9cc9440aa0e2135c18c75681
catherinekimani <catherinekimani882@gmail.com>
2023-02-27 20:57:37 -0800

    Fix bug in code to print correct output

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
