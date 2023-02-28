5b1a918a8a83d7fd348263c538e1757ad6936e47
catherinekimani <catherinekimani882@gmail.com>
2023-02-27 19:21:52 -0800

    Rewrite code to fix error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,8 @@
 #include "main.h"
+
 /**
- * puts2 - function that prints every other character of a string
- * starting with the first character, followed by a new line.
- * @str: Character to be checked
- * Return: 0
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to string
  */
 void puts2(char *str)
 {
