c05aa10bb1384f2c53d5d277fb3e2a1a96b31fd1
catherinekimani <catherinekimani882@gmail.com>
2023-02-28 14:16:09 -0800

    feat: Add _atoi function to convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+# include "main.h"
+/**
+ * _atoi - convert string to interger
+ * @s: pointer to convert
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[0] == '+')
+	{
+		i++;
+	}
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return (sign * result);
+}
