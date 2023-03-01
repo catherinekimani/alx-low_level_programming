5fb2733ab9a4c0d452591a464b78384fd0d8a946
catherinekimani <catherinekimani882@gmail.com>
2023-03-01 06:06:40 -0800

    Feat: Create function to concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - function appends the src string to the dest string
+ * @dest: pointer to character
+ * @src: pointer to charcter
+ * Return: a pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
