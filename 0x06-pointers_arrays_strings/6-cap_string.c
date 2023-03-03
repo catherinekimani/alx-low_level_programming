3349b87bf71e4971596c5f871fc8a8f359f8f941
catherinekimani <catherinekimani882@gmail.com>
2023-03-02 17:19:20 -0800

    Update func

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,8 @@
-# include "main.h"
+#include "main.h"
 /**
  * cap_string - Capitalizes all words of a string
- * @str: string to ba capitalized
+ * @str: The string to be capitalized.
+ *
  * Return: A pointer to the changed string
  */
 char *cap_string(char *str)
@@ -12,6 +13,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
+
 		if (str[i - 1] == ' ' ||
 				str[i - 1] == '\t' ||
 				str[i - 1] == '\n' ||
@@ -27,7 +29,10 @@ char *cap_string(char *str)
 				str[i - 1] == '}' ||
 				i == 0)
 			str[i] -= 32;
+
 		i++;
 	}
+
 	return (str);
 }
+
