5c2c3eee6a7e132343f2de0fab6c19050b56c0b9
catherinekimani <catherinekimani882@gmail.com>
2023-03-02 17:07:49 -0800

    Feat: Create func to capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+# include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: string to ba capitalized
+ * Return: A pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
