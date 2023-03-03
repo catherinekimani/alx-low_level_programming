22fa19d24ad5ee5315e6d0002aa81d5a633e9231
catherinekimani <catherinekimani882@gmail.com>
2023-03-02 17:36:43 -0800

    Feat: Create function to encode string

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
