334a71055792ff2bec3db182ed13430f8db26f5b
catherinekimani <catherinekimani882@gmail.com>
2023-03-10 16:03:46 -0800

    Feat: Create program to print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * program that printsits name
+ * @argc: ARGument count
+ * @argv:ARGument Vector
+ * Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
