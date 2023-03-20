452b73f8a4e11d474f1e5ed964836cf45ee7ffcc
catherinekimani <catherinekimani882@gmail.com>
2023-03-21 01:11:17 +0300

    Feat: Add struct definition and documentation to dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - This struct represents a dog, with a name, age, and owner.
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's ower
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
