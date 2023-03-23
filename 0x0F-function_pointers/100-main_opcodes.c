480b854d4f46662d32aa523d5af7b8bcac324634
catherinekimani <catherinekimani882@gmail.com>
2023-03-23 03:51:57 +0300

    Feat: Create program to print opcodes of it's own main func

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
