91e527ee65feac7036b7d5f7afdeaf65db86e8f8
catherinekimani <catherinekimani882@gmail.com>
2023-04-07 02:36:29 +0300

    Feat: Create func to convert binary no. to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to string of 0 and 1 chars
+ *
+ * Return: Converted or 0 if there is no more chars
+ *         in the string b that is not 0 or 1 0r if b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int str_length = 0;
+
+	if (b[str_length] == '\0')
+		return (0);
+
+	while ((b[str_length] == '0') || (b[str_length] == '1'))
+	{
+		num <<= 1;
+		num += b[str_length] - '0';
+		str_length++;
+	}
+	return (num);
+}
