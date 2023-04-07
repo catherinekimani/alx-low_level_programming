08c9bdbf67618fcca58cc6efcb1a5087d0c4ec2b
catherinekimani <catherinekimani882@gmail.com>
2023-04-07 22:03:17 +0300

    Feat: Create func to count no. of bits needed to be flipped btw two numbers

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - func that returns the no. of bits you would
+ *             need to flip to get from one no. to another.
+ * @n: first no.
+ * @m: second no.
+ *
+ * Return: Required no. of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits = 0;
+	unsigned int xor_res = n ^ m;
+
+	while (xor_res > 0)
+	{
+		bits += (xor_res & 1);
+		xor_res >>= 1;
+	}
+	return (bits);
+}
