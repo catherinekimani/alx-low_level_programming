d0c15f6d36d0bd0105853349fb17941134b4b047
catherinekimani <catherinekimani882@gmail.com>
2023-05-04 01:31:04 +0300

    Ft: Create func to find the loop in a list, add test file to gitignore

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * find_listint_loop - func that finds the loop of a linked list
+ * @head: pointer to the head in the list
+ *
+ * Return: Address of the node where the loop starts or
+ *         Null if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise = head;
+	listint_t *hare = head;
+
+	if (!head)
+		return (NULL);
+
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
+}
