6b30a30fa41afe15020f019f404196d76b27c205
catherinekimani <catherinekimani882@gmail.com>
2023-05-04 00:53:40 +0300

    Ft:Create func to print a linked list,Add test file to gitignore

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - func that prints a listint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num_nodes = 0;
+	listint_t *current_node = (listint_t *)head;
+
+	while (current_node && current_node > current_node->next)
+	{
+		printf("[%p] %d\n", (void *)current_node, current_node->n);
+		current_node = current_node->next;
+		num_nodes++;
+	}
+	if (current_node)
+	{
+		printf("[%p] %d\n", (void *)current_node, current_node->n);
+		printf("-> [%p] %d\n", (void *)current_node->next, current_node->next->n);
+		num_nodes++;
+	}
+	return (num_nodes);
+}
