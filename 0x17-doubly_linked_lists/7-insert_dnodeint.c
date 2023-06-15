d2f935441bdab44f1d0eef27a228fc4dd95d4507
catherinekimani <catherinekimani882@gmail.com>
2023-06-16 02:59:06 +0300

    Refactor code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,38 +11,43 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *current = *h;
-	unsigned int indx = 0;
+	dlistint_t *current;
+	unsigned int indx;
 
-	if (h == NULL)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
+	new_node = NULL;
 	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	else
 	{
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		new_node->next = *h;
-		*h = new_node;
+		current = *h;
+		indx = 1;
+		if (current != NULL)
+			while (current->prev != NULL)
+				current = current->prev;
+		while (current != NULL)
+		{
+			if (indx == idx)
+			{
+				if (current->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = current->next;
+						new_node->prev = current;
+						current->next->prev = new_node;
+						current->next = new_node;
+					}
+				}
+				break;
+			}
+			current = current->next;
+			indx++;
+		}
 	}
-	while (indx < idx - 1)
-	{
-		if (current == NULL)
-			return (NULL);
-		current = current->next;
-		indx++;
-	}
-	if (current == NULL)
-		return (NULL);
-	new_node->next = current->next;
-	new_node->prev = current;
-	if (current->next == new_node)
-		current->next->prev = new_node;
-	current->next = new_node;
+
 	return (new_node);
 }
