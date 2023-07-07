894bef4f0f11d8822c822eca4cdaf04f8a005e2e
catherinekimani <catherinekimani882@gmail.com>
2023-07-08 00:32:26 +0300

    Check betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
 	idx = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[idx];
 	while (current != NULL)
